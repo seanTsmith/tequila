7208e56a54371f1ed3205ab8a2ca9fa33a94e638
Sean Smith <dremoor@gmail.com>
2013-04-04 19:19:45 -0400

    working on test framework

--- test-spec/browser-test-runner.js
+++ test-spec/browser-test-runner.js
@@ -1,54 +1,76 @@
 test.start();
+//test.heading('TEQUILA TEST SPEC', function () {
+//
+//  test.paragraph("Now is the good time for paragraphs.  Now is the good time for paragraphs.  Now is the good time for paragraphs.  Now is the good time for paragraphs.  Now is the good time for paragraphs.  Now is the good time for paragraphs.  Now is the good time for paragraphs.  Now is the good time for paragraphs.  Now is the good time for paragraphs.  Now is the good time for paragraphs.  ")
+//  test.paragraph("Now is the good time for paragraphs.  Now is the good time for paragraphs.  Now is the good time for paragraphs.  Now is the good time for paragraphs.  Now is the good time for paragraphs.  Now is the good time for paragraphs.  Now is the good time for paragraphs.  Now is the good time for paragraphs.  Now is the good time for paragraphs.  Now is the good time for paragraphs.  ")
+//  test.paragraph("Now is the good time for paragraphs.  Now is the good time for paragraphs.  Now is the good time for paragraphs.  Now is the good time for paragraphs.  Now is the good time for paragraphs.  Now is the good time for paragraphs.  Now is the good time for paragraphs.  Now is the good time for paragraphs.  Now is the good time for paragraphs.  Now is the good time for paragraphs.  ")
+//  test.example('Uno', function () {
+//    // First
+//    return 1;
+//  });
+//  test.example('Dos', function () {
+//    // Second
+//    return 2;
+//  });
+//  test.heading('HEADING LEVEL 2', function () {
+//    test.paragraph("Now is the good time for paragraphs.  Now is the good time for paragraphs.  Now is the good time for paragraphs.  Now is the good time for paragraphs.  Now is the good time for paragraphs.  Now is the good time for paragraphs.  Now is the good time for paragraphs.  Now is the good time for paragraphs.  Now is the good time for paragraphs.  Now is the good time for paragraphs.  ")
+//    test.example('three', function () {
+//      // meh
+//    });
+//    test.heading('HEADING LEVEL 3', function () {
+//      test.paragraph("Now is the good time for paragraphs.  Now is the good time for paragraphs.  Now is the good time for paragraphs.  Now is the good time for paragraphs.  Now is the good time for paragraphs.  Now is the good time for paragraphs.  Now is the good time for paragraphs.  Now is the good time for paragraphs.  Now is the good time for paragraphs.  Now is the good time for paragraphs.  ")
+//      test.heading('HEADING LEVEL 4', function () {
+//        test.paragraph("Now is the good time for paragraphs.  Now is the good time for paragraphs.  Now is the good time for paragraphs.  Now is the good time for paragraphs.  Now is the good time for paragraphs.  Now is the good time for paragraphs.  Now is the good time for paragraphs.  Now is the good time for paragraphs.  Now is the good time for paragraphs.  Now is the good time for paragraphs.  ")
+//        test.heading('HEADING LEVEL 5', function () {
+//          test.paragraph("Now is the good time for paragraphs.  Now is the good time for paragraphs.  Now is the good time for paragraphs.  Now is the good time for paragraphs.  Now is the good time for paragraphs.  Now is the good time for paragraphs.  Now is the good time for paragraphs.  Now is the good time for paragraphs.  Now is the good time for paragraphs.  Now is the good time for paragraphs.  ")
+//          test.heading('HEADING LEVEL 6', function () {
+//            test.paragraph("Now is the good time for paragraphs.  Now is the good time for paragraphs.  Now is the good time for paragraphs.  Now is the good time for paragraphs.  Now is the good time for paragraphs.  Now is the good time for paragraphs.  Now is the good time for paragraphs.  Now is the good time for paragraphs.  Now is the good time for paragraphs.  Now is the good time for paragraphs.  ")
+//            test.example('bbb', function () {
+//              // this sample code here
+//              var something = 'asdasdasd';
+//              var nothing;
+//              if (something == nothing) {
+//                console.log("well isn't that special, ok listen this code is long enought to show what margin is needed at most ok???");
+//              }
+//            });
+//          });
+//        });
+//      });
+//    });
+//  });
+//  test.paragraph("This is for real.  This is for real.  This is for real.  This is for real.  This is for real.  This is for real.  This is for real.  This is for real.")
+//  test.heading('PEACE', function () {
+//    test.example('PIPE');
+//  });
+//  test.paragraph("This is for real.  This is for real.  This is for real.  This is for real.  This is for real.  This is for real.  This is for real.  This is for real.")
+//  test.example('aaa');
+//  test.paragraph("This is for real.  This is for real.  This is for real.  This is for real.  This is for real.  This is for real.  This is for real.  This is for real.")
+//  test.example('bbb', function () {
+//    // this example is a bb
+//    var x1 = 1;
+//    var x2 = 2;
+//    // cool
+//    x1 = x2/0;
+//  });
+//
+//
+//});
 test.heading('TEQUILA TEST SPEC', function () {
-
-  test.paragraph("Now is the good time for paragraphs.  Now is the good time for paragraphs.  Now is the good time for paragraphs.  Now is the good time for paragraphs.  Now is the good time for paragraphs.  Now is the good time for paragraphs.  Now is the good time for paragraphs.  Now is the good time for paragraphs.  Now is the good time for paragraphs.  Now is the good time for paragraphs.  ")
-  test.example('Uno', function () {
-    // First
+  test.example('Rule the world');
+  test.example('return 1', 1, function () {
+    // Best test ever
+    return 1;
   });
-  test.example('Dos', function () {
-    // Second
+  test.example('divide by zero', 'spanish inquisition', function () {
+    // Fail on purpose
+    return 1/0;
   });
-  test.heading('HEADING LEVEL 2', function () {
-    test.paragraph("Now is the good time for paragraphs.  Now is the good time for paragraphs.  Now is the good time for paragraphs.  Now is the good time for paragraphs.  Now is the good time for paragraphs.  Now is the good time for paragraphs.  Now is the good time for paragraphs.  Now is the good time for paragraphs.  Now is the good time for paragraphs.  Now is the good time for paragraphs.  ")
-    test.example('three', function () {
-      // meh
-    });
-    test.heading('HEADING LEVEL 3', function () {
-      test.paragraph("Now is the good time for paragraphs.  Now is the good time for paragraphs.  Now is the good time for paragraphs.  Now is the good time for paragraphs.  Now is the good time for paragraphs.  Now is the good time for paragraphs.  Now is the good time for paragraphs.  Now is the good time for paragraphs.  Now is the good time for paragraphs.  Now is the good time for paragraphs.  ")
-      test.heading('HEADING LEVEL 4', function () {
-        test.paragraph("Now is the good time for paragraphs.  Now is the good time for paragraphs.  Now is the good time for paragraphs.  Now is the good time for paragraphs.  Now is the good time for paragraphs.  Now is the good time for paragraphs.  Now is the good time for paragraphs.  Now is the good time for paragraphs.  Now is the good time for paragraphs.  Now is the good time for paragraphs.  ")
-        test.heading('HEADING LEVEL 5', function () {
-          test.paragraph("Now is the good time for paragraphs.  Now is the good time for paragraphs.  Now is the good time for paragraphs.  Now is the good time for paragraphs.  Now is the good time for paragraphs.  Now is the good time for paragraphs.  Now is the good time for paragraphs.  Now is the good time for paragraphs.  Now is the good time for paragraphs.  Now is the good time for paragraphs.  ")
-          test.heading('HEADING LEVEL 6', function () {
-            test.paragraph("Now is the good time for paragraphs.  Now is the good time for paragraphs.  Now is the good time for paragraphs.  Now is the good time for paragraphs.  Now is the good time for paragraphs.  Now is the good time for paragraphs.  Now is the good time for paragraphs.  Now is the good time for paragraphs.  Now is the good time for paragraphs.  Now is the good time for paragraphs.  ")
-            test.example('bbb', function () {
-              // this sample code here
-              var something = 'asdasdasd';
-              var nothing;
-              if (something == nothing) {
-                console.log("well isn't that special, ok listen this code is long enought to show what margin is needed at most ok???");
-              }
-            });
-          });
-        });
-      });
-    });
+  test.xexample('answer to the universe', 'fix this someday', function () {
+    return (6((1/0)-(1/0))^2)/(((1/0)^(1/0))/7);
   });
-  test.paragraph("This is for real.  This is for real.  This is for real.  This is for real.  This is for real.  This is for real.  This is for real.  This is for real.")
-  test.heading('PEACE', function () {
-    test.example('PIPE');
+  test.example('expect nothing to be undefined', ReferenceError('nothing is not defined') , function () {
+    return nothing;
   });
-  test.paragraph("This is for real.  This is for real.  This is for real.  This is for real.  This is for real.  This is for real.  This is for real.  This is for real.")
-  test.example('aaa');
-  test.paragraph("This is for real.  This is for real.  This is for real.  This is for real.  This is for real.  This is for real.  This is for real.  This is for real.")
-  test.example('bbb', function () {
-    // this example is a bb
-    var x1 = 1;
-    var x2 = 2;
-    // cool
-  });
-
-
 });
 test.stop();
 test.render();
--- test-spec/test-tequila.js
+++ test-spec/test-tequila.js
@@ -2,21 +2,17 @@
  * tequila
  * test-tequila
  */
-
-
-var TestNode = function (nodeType, level, levelText, text, func, exampleNumber, expectException, expectedValue) {
+var TestNode = function (nodeType, level, levelText, text, func, exampleNumber, deferedExample, expectedValue) {
   this.nodeType = nodeType; // nodeType 1 char string: H)eading P)aragraph E)xample E(X)eption
   this.level = level;
   this.levelText = levelText;
   this.text = text;
   this.func = func;
   this.exampleNumber = exampleNumber;
-  this.expectException = expectException;
+  this.deferedExample = deferedExample;
   this.expectedValue = expectedValue;
   return this;
 };
-
-
 var test = {};
 test.converter = new Markdown.Converter();
 test.log = function (txt) {
@@ -47,11 +43,15 @@ test.paragraph = function (text) {
   this.nodes.push(new TestNode('P', this.headingLevel + 1, this.outlineLabel, text));
   this.log('test.paragraph ' + text)
 };
-test.example = function (text, func, expect) {
+test.example = function (text, expect, func) {
   this.exampleNumber++;
   this.log('test.example ' + this.outlineLabel + ' ' + text)
-  if (func) func();
-  this.nodes.push(new TestNode('e', this.headingLevel + 1, this.outlineLabel, text, func, this.exampleNumber));
+  this.nodes.push(new TestNode('e', this.headingLevel + 1, this.outlineLabel, text, func, this.exampleNumber, false, expect));
+};
+test.xexample = function (text, expect, func) {
+  this.exampleNumber++;
+  this.log('xexample.example ' + this.outlineLabel + ' ' + text)
+  this.nodes.push(new TestNode('e', this.headingLevel + 1, this.outlineLabel, text, func, this.exampleNumber, true, expect));
 };
 test.exception = function (text, func, Error) {
   this.exampleNumber++;
@@ -65,7 +65,32 @@ test.run = function (resultsCallback) {
   this.log('test.run')
 };
 test.render = function (options) {
-  this.log('test.render')
+  this.log('test.render');
+
+  test.countTests = 0;
+  test.countPass = 0;
+  test.countFail = 0;
+  test.countDefer = 0;
+
+  // Fixed Header Div
+  var headerDiv = document.createElement("div");
+  headerDiv.style.position = 'fixed';
+  headerDiv.style.top = '0px';
+  headerDiv.style.margin = 'auto';
+  headerDiv.style.zIndex = '100000';
+  headerDiv.style.width = '100%';
+  headerDiv.style.background = '#AA4'; // pass color
+//  headerDiv.style.background = '#6C7'; // pass color
+  var stats = document.createElement("p");
+  stats.id = "stats";
+  stats.innerHTML = test.converter.makeHtml('**tequila**');
+  stats.align = 'center';
+  stats.style.padding = '0px';
+  stats.style.margin = '0px';
+  headerDiv.appendChild(stats);
+  document.body.appendChild(headerDiv);
+
+  // Outer & Inner Div to center content
   var outerDiv = document.createElement("div");
   outerDiv.style.width = "100%";
   document.body.appendChild(outerDiv);
@@ -73,47 +98,88 @@ test.render = function (options) {
   innerDiv.style.width = "1000px";
   innerDiv.style.margin = "0 auto";
   outerDiv.appendChild(innerDiv);
-
   for (i in test.nodes) {
     switch (test.nodes[i].nodeType) {
       case 'h':
         var p = document.createElement("h" + test.nodes[i].level);
-        var lt =  test.nodes[i].levelText;
-        if (lt=='1.') lt = '';
-        if (lt.length>2) lt = lt.substring(0,lt.length-1);
+        var lt = test.nodes[i].levelText;
+        if (lt == '1.') lt = '';
+        if (lt.length > 2) lt = lt.substring(0, lt.length - 1);
         p.innerHTML = lt + ' ' + test.nodes[i].text;
         innerDiv.appendChild(p);
         break;
+
       case 'P':
         var p = document.createElement("p");
         p.innerHTML = test.converter.makeHtml(test.nodes[i].text);
         innerDiv.appendChild(p);
-
-//   var text = "Markdown *rocks*.";
-//
-//
-//   var html = converter.makeHtml(text);
-//
-//   alert(html);
-
         break;
+
       case 'e':
         var caption = document.createElement("caption");
         caption.innerHTML = '<caption>' + 'EXAMPLE #' + test.nodes[i].exampleNumber + ' ' + test.nodes[i].text + '</caption>'
         innerDiv.appendChild(caption);
         var pre = document.createElement("pre");
         pre.className = "prettyprint";
-        if (test.nodes[i].func) {
-          pre.innerHTML = '<code>' + test.formatCode(test.nodes[i].func) + '</code>';
+        test.countTests++;
+        if (!test.nodes[i].deferedExample && test.nodes[i].func) {
+          var test_Results = test.callTestCode(test.nodes[i].func);
+          var exampleCode = '';
+          exampleCode += test.formatCode(test.nodes[i].func);
+          if (test_Results.toString() !== test.nodes[i].expectedValue.toString()) {
+            if (!test.countFail) headerDiv.style.background = '#F33'; // fail color color
+            test.countFail++;
+            pre.style.background = "#fcc"; // red
+            exampleCode += '✘ <b>RETURNED: ' + test.expressionInfo(test_Results) + '\n  EXPECTED: ' + test.expressionInfo(test.nodes[i].expectedValue) + '</b>'; // ✘
+          } else {
+            test.countPass++;
+            pre.style.background = "#cfc"; // green
+            if (test.wasThrown) {
+              exampleCode += '✓ <b>error thrown as expected (' + test_Results + ')</b>'; // ✘
+            } else {
+              exampleCode += '✓ <b>returns ' + test_Results + ' as expected</b>'; // ✘
+            }
+          }
+          pre.innerHTML = '<code>' + exampleCode + '</code>';
         } else {
-          pre.innerHTML = '<code> TODO: write some code that rocks.</code>';
+          test.countDefer++;
+          pre.style.background = "#ffc"; // yellow
+          if (test.nodes[i].func) {
+            exampleCode = test.formatCode(test.nodes[i].func);
+            exampleCode += '✍ <b>(test disabled)</b>';
+            pre.innerHTML = exampleCode;
+          } else {
+            pre.innerHTML = '<code> TODO: write some code that rocks.</code>';
+          }
         }
         innerDiv.appendChild(pre);
+        test.updateStats();
         break;
     }
   }
+  if (!test.countFail) headerDiv.style.background = '#6C7'; // pass color
 };
+test.updateStats = function () {
+  var stats = document.getElementById("stats");
+  stats.innerHTML = test.converter.makeHtml('**tequila** tests: **'+test.countTests+'** pass: **'+test.countPass+'** fail: **'+test.countFail+'** defer: **'+test.countDefer+'**');
+}
+test.expressionInfo = function (expr) {
 
+  if (typeof expr == 'string') {
+    return '"' + expr.replace(/"/g, '\\\"') + '"';
+    //str = '"' + expr.replace(/"/g, '\"') + '"'
+  }
+  return expr;
+}
+  test.callTestCode = function (func) {
+  try {
+    test.wasThrown = false;
+    return func();
+  } catch (e) {
+    test.wasThrown = true;
+    return e;
+  }
+}
 test.formatCode = function (txt) {
   var lines = [];
   var spaces = [];
