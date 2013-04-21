0ef38144d43e4d1b76387e839c447a5c341ba571
Sean Smith <dremoor@gmail.com>
2013-04-21 18:18:49 -0400

    need a model clone apparently

--- model-core/memory-store-model.js
+++ model-core/memory-store-model.js
@@ -15,20 +15,52 @@ var MemoryStore = function (args) {
 };
 MemoryStore.prototype = T.inheritPrototype(Store.prototype);
 // Methods
-MemoryStore.prototype.getModel = function (model, callBack) {
+MemoryStore.prototype.getModel = function (model, callBack, self) {
   if (!(model instanceof Model)) throw new Error('argument must be a Model');
   if (model.getValidationErrors().length) throw new Error('model has validation errors');
   if (!model.attributes[0].value) throw new Error('ID not set');
   if (typeof callBack != "function") throw new Error('callback required');
-  callBack(model, new Error('model not found in store'));
+  // Find model in memorystore, error out if can't find
+  var modelIndex = -1;
+  for (var i = 0; i < this.data.length; i++) if (this.data[i][0] == model.modelType) modelIndex = i;
+  if (modelIndex < 0) {
+    callBack(model, new Error('model not found in store'), self);
+    return;
+  }
+  // Find the ID now
+  var instanceIndex = -1;
+  var id = model.getAttributeValue('id');
+  var storedPair = this.data[modelIndex][1];
+  for (var i = 0; instanceIndex<0 && i < storedPair.length; i++) if (storedPair[i][0] == id) instanceIndex = i;
+  if (instanceIndex < 0) {
+    callBack(model, new Error('id not found in store'), self);
+    return;
+  }
+  callBack(storedPair[instanceIndex][1],undefined,self);
 };
-MemoryStore.prototype.putModel = function (model, callBack) {
+MemoryStore.prototype.putModel = function (model, callBack, self) {
   if (!(model instanceof Model)) throw new Error('argument must be a Model');
   if (model.getValidationErrors().length) throw new Error('model has validation errors');
   if (typeof callBack != "function") throw new Error('callback required');
   var id = model.getAttributeValue('ID');
   if (id) {
-    callBack(model, new Error('model not found in store'));
+    // Find model in memorystore, error out if can't find
+    var modelIndex = -1;
+    for (var i = 0; i < this.data.length; i++) if (this.data[i][0] == model.modelType) modelIndex = i;
+    if (modelIndex < 0) {
+      callBack(model, new Error('model not found in store'), self);
+      return;
+    }
+    // Find the ID now
+    var instanceIndex = -1;
+    var id = model.getAttributeValue('id');
+    var storedPair = this.data[modelIndex][1];
+    for (var i = 0; instanceIndex<0 && i < storedPair.length; i++) if (storedPair[i][0] == id) instanceIndex = i;
+    if (instanceIndex < 0) {
+      callBack(model, new Error('id not found in store'), self);
+      return;
+    }
+    callBack(storedPair[instanceIndex][1],undefined,self);
   } else {
     // Find model in memorystore, add if not found
     var modelIndex = -1;
@@ -41,7 +73,7 @@ MemoryStore.prototype.putModel = function (model, callBack) {
     var newID = ++this.idCounter;
     model.setAttributeValue('id',newID);
     this.data[modelIndex][1].push([newID, model]);
-    callBack(model);
+    callBack(model,undefined,self);
   }
 };
 MemoryStore.prototype.deleteModel = function (model, callBack) {
--- test-spec/integration/test-store-integration.js
+++ test-spec/integration/test-store-integration.js
@@ -7,37 +7,85 @@ test.runnerStoreIntegration = function () {
     test.paragraph('');
     test.heading('CRUD (Create Read Update Delete)', function () {
       test.example('Exercise and verify CRUD functionality.', undefined, function () {
-        // Create a memory store to test
-        var store = new MemoryStore();
-        // Futbol Americano
-        var Team = function (args) {
+
+        // setup store and stooge class
+        this.store = new MemoryStore();
+        this.Stooge = function (args) {
           Model.call(this, args);
-          this.modelType = "Team";
-          this.attributes.push(new Attribute('division'));
-          this.attributes.push(new Attribute('club'));
-          this.attributes.push(new Attribute('stadium'));
+          this.modelType = "Stooge";
+          this.attributes.push(new Attribute('name'));
         };
-        Team.prototype = T.inheritPrototype(Model.prototype);
-        var Player = function (args) {
-          Model.call(this, args);
-          this.modelType = "Player";
-          this.attributes.push(new Attribute('player'));
-          this.attributes.push(new Attribute('position'));
-          this.attributes.push(new Attribute('team', 'ID'));
-        };
-        Player.prototype = T.inheritPrototype(Model.prototype);
-        var ravens = new Team();
-        ravens.setAttributeValue('club', 'Ravens');
-        store.putModel(ravens, function (model, error) {
-          if (!error) {
-            var flacco = new Player();
-            flacco.setAttributeValue('player', 'Joe Flacco');
-            flacco.setAttributeValue('team', model.getAttributeValue('id'));
-            store.putModel(flacco, function (model, error) {
-              test.show(model);
-            });
+        this.Stooge.prototype = T.inheritPrototype(Model.prototype);
+
+        // create initial stooges
+        this.moe = new this.Stooge();
+        this.moe.setAttributeValue('name', 'Moe');
+        this.larry = new this.Stooge();
+        this.larry.setAttributeValue('name', 'Larry');
+        this.shemp = new this.Stooge();
+        this.shemp.setAttributeValue('name', 'Shemp');
+
+        // IDs after stored will be here
+        this.stoogeIDsStored = [];
+        this.stoogesRetrieved = [];
+
+        // store the stooges
+        this.store.putModel(this.moe, stoogeStored, this); // todo unit test this / self
+        this.store.putModel(this.larry, stoogeStored, this);
+        this.store.putModel(this.shemp, stoogeStored, this);
+
+        // callback after storing stooges
+        function stoogeStored(model, error, self) {
+          if (typeof error != 'undefined') throw error;
+          self.stoogeIDsStored.push(model.getAttributeValue('id'));
+          if (self.stoogeIDsStored.length == 3) {
+            // Now that first 3 stooges are stored lets retrieve and verify
+            var actors = [];
+            for (var i = 0; i < 3; i++) {
+              actors.push(new self.Stooge());
+              actors[i].setAttributeValue('id', self.stoogeIDsStored[i]);
+              self.store.getModel(actors[i], stoogeRetrieved, self);
+            }
           }
-        });
+        }
+
+        // callback after retrieving stored stooges
+        function stoogeRetrieved(model, error, self) {
+          if (typeof error != 'undefined') throw error;
+          self.stoogesRetrieved.push(model);
+          if (self.stoogesRetrieved.length == 3) {
+            // Now we have stored and retrieved (via IDs into new objects).  So verify the stooges made it
+            var stoogez = [];
+            for (var i=0; i<3; i++) stoogez.push(self.stoogesRetrieved[i].getAttributeValue('name'));
+            test.assertion(T.contains(stoogez,'Moe'));
+            test.assertion(T.contains(stoogez,'Larry'));
+            test.assertion(T.contains(stoogez,'Shemp'));
+            // Replace Shemp with Curly
+            for (var i=0; i<3; i++) {
+              if (self.stoogesRetrieved[i].getAttributeValue('name') == 'Shemp') {
+                self.stoogesRetrieved[i].setAttributeValue('name','Curly');
+                self.store.putModel(self.stoogesRetrieved[i], stoogeStored2, self);
+              }
+            }
+          }
+        }
+        // callback after storing changed stooge
+        function stoogeStored2(model, error, self) {
+          if (typeof error != 'undefined') throw error;
+          // See if change test passed
+          test.show(model.getAttributeValue('name'));
+          test.assertion(model.getAttributeValue('name')=='Curly Clone'); // fucked because memorystore needs to clone models first!
+          // Now test delete
+//          self.store.deleteModel(model,stoogeDeleted)
+        }
+
+        // callback after storing changed stooge
+        function stoogeDeleted(model, error, self) {
+//          if (error) throw error;
+//          // See if change test passed
+//          test.assertion(model.getAttributeValue('name')=='Curly');
+        }
+
       });
     });
   });
