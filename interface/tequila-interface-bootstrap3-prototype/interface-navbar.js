d2aad458784b10748da6a1a77bfd79e3b1390a34
Sean Smith <dremoor@gmail.com>
2013-10-26 16:25:22 -0400

    bck to work :()

---
+++ interface/tequila-interface-bootstrap3-prototype/interface-navbar.js
@@ -0,0 +1,173 @@
+/**
+ * tequila
+ * interface-navbar
+ */
+
+// -------------------------------------------------------------------------------------------------------------------
+// Render NavBar
+// -------------------------------------------------------------------------------------------------------------------
+myInterface.renderNavBar = function () {
+
+  // Main navbar
+  myInterface.navBar = document.createElement("header");
+  myInterface.navBar.id = "navBar";
+  myInterface.navBar.className = "navbar navbar-default navbar-fixed-top";
+//  myInterface.navBar.className = "navbar navbar-inverse navbar-fixed-top";
+  myInterface.navBar.setAttribute('role', 'banner');
+  document.body.appendChild(myInterface.navBar);
+
+  // Add a gap below navbar if not small view -->
+  var navBreak = document.createElement("br");
+  navBreak.className = "hidden-sm";
+  document.body.appendChild(navBreak);
+  document.body.appendChild(navBreak);
+  document.body.appendChild(navBreak);
+  document.body.appendChild(navBreak);
+  document.body.appendChild(navBreak);
+
+  // Container so bootstrap centers
+  myInterface.navContainer = document.createElement("div");
+  myInterface.navContainer.id = "navContainer";
+  myInterface.navContainer.className = "container";
+  myInterface.navBar.appendChild(myInterface.navContainer);
+
+  // Responsive header
+  myInterface.navResponsiveHeader = document.createElement("div");
+  myInterface.navResponsiveHeader.id = "navResponsiveHeader";
+  myInterface.navResponsiveHeader.className = "navbar-header";
+  myInterface.navContainer.appendChild(myInterface.navResponsiveHeader);
+
+  // Navbar toggle button when small presentation
+  var iconBar = '<span class="icon-bar"></span>';
+  myInterface.navBarToggle = document.createElement("button");
+  myInterface.navBarToggle.id = "navBarToggle";
+  myInterface.navBarToggle.className = "navbar-toggle";
+  myInterface.navBarToggle.setAttribute('type', 'button');
+  myInterface.navBarToggle.setAttribute('data-toggle', 'collapse');
+  myInterface.navBarToggle.setAttribute('data-target', '.navbar-collapse');
+  myInterface.navBarToggle.innerHTML = iconBar + iconBar + iconBar;
+  myInterface.navResponsiveHeader.appendChild(myInterface.navBarToggle);
+
+  // Brand
+  myInterface.navBarBrand = document.createElement("a");
+  myInterface.navBarBrand.id = "navBarBrand";
+  myInterface.navBarBrand.className = "navbar-brand";
+  myInterface.navBarBrand.innerHTML = '<a href="javascript:alert(\'wtf\')">' + 'tequila.js' + '</a>';
+  myInterface.navResponsiveHeader.appendChild(myInterface.navBarBrand);
+
+//  <li class="active"><a style="display: block; text-align: center;" href="javascript:loadStore()">TRY AGAIN</a></li>
+
+
+  // Collapsible navbar
+  myInterface.collapsibleNavBar = document.createElement("div");
+  myInterface.collapsibleNavBar.id = "collapsibleNavBar";
+  myInterface.collapsibleNavBar.className = "navbar-collapse collapse";
+  myInterface.collapsibleNavBar.setAttribute('role', 'navigation');
+  myInterface.navContainer.appendChild(myInterface.collapsibleNavBar);
+
+  // navbar list
+  myInterface.navList = document.createElement("ul");
+  myInterface.navList.id = "navList";
+  myInterface.navList.className = "nav navbar-nav";
+  myInterface.collapsibleNavBar.appendChild(myInterface.navList);
+
+  myInterface.stooges = myInterface.renderNavBarListMenu(myInterface.navList, 'Stooges');
+  myInterface.renderNavBarListItem(myInterface.stooges, 'Moe');
+  myInterface.renderNavBarListItem(myInterface.stooges, 'Larry');
+  myInterface.renderNavBarListItem(myInterface.stooges, 'Curly');
+
+  myInterface.fruit = myInterface.renderNavBarListMenu(myInterface.navList, 'Fruit');
+  myInterface.renderNavBarListItem(myInterface.fruit, 'Apple');
+  myInterface.renderNavBarListItem(myInterface.fruit, 'Banana');
+  myInterface.renderNavBarListItem(myInterface.fruit, 'Cherry');
+  myInterface.renderNavBarListItem(myInterface.fruit, 'Peach');
+
+  myInterface.superhero = myInterface.renderNavBarListMenu(myInterface.navList, 'Super Heroes');
+  myInterface.renderNavBarListItem(myInterface.superhero, 'Superman');
+  myInterface.renderNavBarListItem(myInterface.superhero, 'Batman');
+  myInterface.renderNavBarListItem(myInterface.superhero, 'Spiderman');
+
+
+  // login right justified
+  myInterface.navListRight = document.createElement("ul");
+  myInterface.navListRight.id = "navListRight";
+  myInterface.navListRight.className = "nav navbar-nav navbar-right";
+  myInterface.collapsibleNavBar.appendChild(myInterface.navListRight);
+
+  myInterface.user = myInterface.renderNavBarListMenu(myInterface.navListRight, 'Clark Kent');
+  myInterface.renderNavBarListItem(myInterface.user, 'Logout', 'glyphicon-log-out');
+
+  // Search
+  myInterface.navSearch = document.createElement("form");
+  myInterface.navSearch.className = "navbar-form navbar-right";
+  myInterface.navSearch.innerHTML = '<div class="form-group">' +
+    '<input type="text" class="form-control" placeholder="Search"></div>' +
+    '<a type="button" href="javascript:myInterface.navPicked(\'Search\')" class="btn btn-default"><span class="glyphicon glyphicon-search"></span></a>';
+  myInterface.collapsibleNavBar.appendChild(myInterface.navSearch);
+
+//  // Collapsible navbar right
+//  myInterface.collapsibleRightNavBar = document.createElement("nav");
+//  myInterface.collapsibleRightNavBar.id = "collapsibleRightNavBar";
+//  myInterface.collapsibleRightNavBar.className = "navbar-collapse collapse navbar-right";
+//  myInterface.collapsibleRightNavBar.setAttribute('role', 'navigation');
+//  myInterface.navContainer.appendChild(myInterface.collapsibleRightNavBar);
+//
+//  // navbar list
+//  myInterface.navListRight = document.createElement("ul");
+//  myInterface.navListRight.id = "navListRight";
+//  myInterface.navListRight.className = "nav navbar-nav";
+//  myInterface.collapsibleRightNavBar.appendChild(myInterface.navListRight);
+//
+//  myInterface.user = myInterface.renderNavBarListMenu(myInterface.navListRight, 'guest');
+
+
+
+//  // fucking with some pull-right shit
+//  myInterface.cart = document.createElement("a");
+//  myInterface.cart.setAttribute('type', 'button');
+//  myInterface.cart.className = "btn btn-default navbar-btn pull-right";
+//  myInterface.cart.innerHTML = '<span class="glyphicon glyphicon-log-out"></span> logout'
+//  myInterface.collapsibleNavBar.appendChild(myInterface.cart);
+
+
+};
+
+// -------------------------------------------------------------------------------------------------------------------
+// Render NavBar List Item
+// -------------------------------------------------------------------------------------------------------------------
+myInterface.navPicked = function (name) {
+  myInterface.renderPanel('Panel ' + name, 'default');
+};
+
+// -------------------------------------------------------------------------------------------------------------------
+// Render NavBar List Item
+// -------------------------------------------------------------------------------------------------------------------
+myInterface.renderNavBarListItem = function (parent, name, glyph) {
+  var listItem = document.createElement('li');
+  console.log(parent + ' ' + name)
+  if (glyph) {
+    listItem.innerHTML = '<a href="javascript:myInterface.navPicked(\'' + name + '\')">' +
+      '<span class="glyphicon ' + glyph + '"></span> ' +
+      '' + name + '</a>';
+  } else {
+    listItem.innerHTML = '<a href="javascript:myInterface.navPicked(\'' + name + '\')">' + name + '</a>';
+  }
+  parent.appendChild(listItem);
+};
+
+// -------------------------------------------------------------------------------------------------------------------
+// Render NavBar List Menu
+// -------------------------------------------------------------------------------------------------------------------
+myInterface.renderNavBarListMenu = function (parent, name) {
+
+  var dropDown = document.createElement('li');
+  dropDown.className = "dropdown";
+  dropDown.innerHTML = '<a href="#" class="dropdown-toggle" data-toggle="dropdown">' + name + '<b class="caret"></b></a>'
+  parent.appendChild(dropDown);
+
+  var dropDownMenu = document.createElement('ul');
+  dropDownMenu.className = "dropdown-menu";
+  dropDown.appendChild(dropDownMenu);
+
+  return dropDownMenu;
+};
---
+++ interface/tequila-interface-bootstrap3-prototype/interface-panel.js
@@ -0,0 +1,99 @@
+/**
+ * tequila
+ * interface-panel.js
+ */
+
+// -------------------------------------------------------------------------------------------------------------------
+// Render Panel
+// -------------------------------------------------------------------------------------------------------------------
+myInterface.renderPanel = function (name, panelType /* default primary success info warning danger */) {
+
+  panelType = panelType || "default";
+
+  myInterface.eleCount++;
+
+  // Container so bootstrap centers
+  if (!myInterface.panelContainer) {
+    myInterface.panelContainer = document.createElement("div");
+    myInterface.panelContainer.id = "panelContainer";
+    myInterface.panelContainer.className = "container";
+    document.body.appendChild(myInterface.panelContainer);
+  }
+
+  var newPanel = document.createElement("div");
+  newPanel.className = "panel panel-" + panelType;
+  newPanel.id = "panel" + myInterface.eleCount;
+  if (myInterface.panelContainer.hasChildNodes()) {
+    var firstBorn = myInterface.panelContainer.firstChild;
+    myInterface.panelContainer.insertBefore(newPanel, firstBorn);
+  } else {
+    myInterface.panelContainer.appendChild(newPanel);
+  }
+
+  var panelHeading = document.createElement("div");
+  panelHeading.className = "panel-heading";
+  newPanel.appendChild(panelHeading);
+
+  var panelTitle = document.createElement("h3");
+  panelTitle.className = "panel-title";
+  panelTitle.innerHTML = name;
+  panelHeading.appendChild(panelTitle);
+
+  var panelCloseButton = document.createElement("a");
+  panelCloseButton.innerHTML = '<span class="glyphicon glyphicon-remove panel-glyphs pull-right"></span>';
+  panelCloseButton.setAttribute('href', "javascript:myInterface.panelClose(" + myInterface.eleCount + ")");
+  panelTitle.appendChild(panelCloseButton);
+
+  var panelContractButton = document.createElement("a");
+  panelContractButton.id = "panelContractButton" + myInterface.eleCount;
+  panelContractButton.innerHTML = '<span class="glyphicon glyphicon-chevron-down panel-glyphs pull-right"></span>';
+  panelContractButton.setAttribute('href', "javascript:myInterface.panelContract(" + myInterface.eleCount + ")");
+  panelTitle.appendChild(panelContractButton);
+
+  var panelExpandButton = document.createElement("a");
+  panelExpandButton.id = "panelExpandButton" + myInterface.eleCount;
+  panelExpandButton.innerHTML = '<span class="glyphicon glyphicon-chevron-up panel-glyphs pull-right"></span>';
+  panelExpandButton.setAttribute('href', "javascript:myInterface.panelExpand(" + myInterface.eleCount + ")");
+  panelTitle.appendChild(panelExpandButton);
+
+  var spam = 'This is the body of the panel for ' + name + '.  ';
+  spam += spam;
+  spam += spam;
+  spam += spam;
+  spam += spam;
+
+  var panelBody = document.createElement("div");
+  panelBody.className = "panel-body";
+  panelBody.id = "panelBody" + myInterface.eleCount;
+  panelBody.innerHTML = '<p>' + spam  + '</p>' + '<p>' + spam  + '</p>';
+  newPanel.appendChild(panelBody);
+
+  $('#panelExpandButton' + myInterface.eleCount).hide();
+
+};
+
+// -------------------------------------------------------------------------------------------------------------------
+// Panel Close
+// -------------------------------------------------------------------------------------------------------------------
+myInterface.panelClose = function (num) {
+  var panelID = 'panel' + num;
+  var panel = document.getElementById(panelID);
+  myInterface.panelContainer.removeChild(panel);
+};
+
+// -------------------------------------------------------------------------------------------------------------------
+// Panel Expand
+// -------------------------------------------------------------------------------------------------------------------
+myInterface.panelExpand = function (num) {
+  $('#panelBody' + num).show();
+  $('#panelExpandButton' + num).hide();
+  $('#panelContractButton' + num).show();
+};
+// -------------------------------------------------------------------------------------------------------------------
+// Panel Contract
+// -------------------------------------------------------------------------------------------------------------------
+myInterface.panelContract = function (num) {
+  $('#panelBody' + num).hide();
+  $('#panelExpandButton' + num).show();
+  $('#panelContractButton' + num).hide();
+};
