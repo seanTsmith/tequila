f88f43218139361918e436dd0de61677bd3c1ae1
Sean Smith <dremoor@gmail.com>
2013-12-24 14:41:01 -0500

    Sample App brand working

--- samples/bootstrap3-panels-interface-sample/app.js
+++ samples/bootstrap3-panels-interface-sample/app.js
@@ -4,20 +4,26 @@
  */
 
 var app = new Application();
-app.set('brand', 'Sample App');
+app.set('brand', 'tgiCloud');
 
 var b3p = new Bootstrap3PanelInterface();
+app.setInterface(b3p);
+
+// App menu
 var menu = new Presentation();
 menu.set('name', 'Main Menu');
 menu.set('contents', [
-  new Command({name: 'Eat'}),
+  new Command({name: 'Stooges', type: 'Menu', contents: [
+    'The Three Stooges',
+    '-',
+    new Command({name: 'Moe'}),
+    new Command({name: 'Larry'}),
+    new Command({name: 'Curly'})
+  ]}),
   new Command({name: 'More'}),
   new Command({name: 'Chiken'})
 ]);
-
-
 app.setPresentation(menu);
-app.setInterface(b3p);
 
 $(document).ready(function () {
   app.start(function (stuff) {
@@ -25,4 +31,3 @@ $(document).ready(function () {
   });
   b3p.mockRequest(new Request('dude'));
 });
-
